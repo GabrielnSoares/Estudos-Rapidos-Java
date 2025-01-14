680a42b9cf241d85030adc893f82a8529e7b30a0
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-13 21:19:19 -0300

    Estudos de Java Streams

---
+++ src/br/gabnsoares/dev/streams/Main.java
@@ -0,0 +1,29 @@
+package br.gabnsoares.dev.streams;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = List.of("Bruno", "Mathias", "Gabriel", "Rogerio");
+
+        var namesContaisA = names.stream()
+                .filter(n -> n.contains("a"))
+                .toList();
+
+        System.out.println(namesContaisA);
+
+/*        var numbers =  IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+         List -> List -> List =list (reduced)
+        numbers.reduce(0, (reduced, each) -> {
+            System.out.println("Reduced: " + reduced);
+            System.out.println("Each: " + each);
+            System.out.println("----------------");
+            return reduced + each;
+
+
+        });*/
+    }
+}
